b575d3ea0bae64e70ffdf71408eec8d49411fe01
Vivek Singh <vivek@ubuntu.(none)>
2014-02-26 18:48:48 +0530

    Changes for Create through JSON

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,21 @@
 class Video < ActiveRecord::Base
   belongs_to :device
+  
+  validates :device, presence: true
+  
+  before_validation :set_device, on: :create
+  
+  def device_imei=(device_imei)
+    @device_imei = device_imei
+  end
+
+  def device_imei
+    @device_imei
+  end
+
+  def set_device
+    if !device_id.present? && device_imei.present?
+      self.device_id = Device.find_by_imei(device_imei).id
+    end
+  end
 end
