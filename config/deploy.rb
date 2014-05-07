b1d242e1fb2c6df69d04c7690e1871c080b30386
Vivek Singh <vivek@ubuntu.(none)>
2014-05-07 16:07:35 +0530

    Deploy Changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,9 +28,8 @@ namespace :deploy do
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{current_path}/uploads"
     run "mkdir -p #{shared_path}/config"
-    run "mkdir -p #{shared_path}/uploads"
-    run "mkdir -p #{shared_path}/public/uploads"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
